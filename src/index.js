b2e299e8f739e7f80c95bc74827d95b48a8bd120
Pritul Singh <pritulsingh225@gmail.com>
2026-05-28 10:56:22 +0530

    Setting up some prerequisites

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+    const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT || 8000}`);
+    });
+
+    server.on("error", (error) =>{
+        console.log("ERROR: ",error);
+        throw error
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!",err);
+})
+
+
+
+
+
+
+
+
+// this is useful for small apps and testing only not when need to build large production apps.
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
